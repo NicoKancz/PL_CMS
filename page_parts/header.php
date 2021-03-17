101a3fd7f7c3a9910b0967d09fb2f9c459373a63
MrNico112 <MrNico112@users.noreply.github.com>
2021-03-17 18:12:49 +0100

    Made TODO's for the main.php(mockup & containers) and footer.php, fixed paths(css,links,img) from header.php.

--- page_parts/header.php
+++ page_parts/header.php
@@ -4,26 +4,26 @@
         <meta content="text/html" charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PL-CMS</title>
-        <link href="./css/myStyle.css" rel="stylesheet">
+        <link href="/PL_CMS/css/myStyle.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <header>
             <nav>
-                <h1 id="logo">Logo</h1>
+                <a href="/PL_CMS/index.php"><h1 id="logo">Logo</h1></a>
                 <ul id="categories">
-                    <li><a href="./categories/php.php">PHP</a></li>
-                    <li><a href="./categories/javascript.php">JavaScript</a></li>
-                    <li><a href="./categories/python.php">Python</a></li>
-                    <li><a href="./categories/java.php">Java</a></li>
-                    <li><a href="./categories/csharp.php">C#</a></li>
-                    <li><a href="./categories/perl.php">Perl</a></li>
-                    <li><a href="./categories/ruby.php">Ruby</a></li>
-                    <li><a href="./categories/cplusplus.php">C++</a></li>
+                    <li><a href="/PL_CMS/categories/php.php">PHP</a></li>
+                    <li><a href="/PL_CMS/javascript.php">JavaScript</a></li>
+                    <li><a href="/PL_CMS/python.php">Python</a></li>
+                    <li><a href="/PL_CMS/java.php">Java</a></li>
+                    <li><a href="/PL_CMS/csharp.php">C#</a></li>
+                    <li><a href="/PL_CMS/perl.php">Perl</a></li>
+                    <li><a href="/PL_CMS/ruby.php">Ruby</a></li>
+                    <li><a href="/PL_CMS/cplusplus.php">C++</a></li>
                 </ul>
                 <form method="post" action="search.php" id="search">
                     <input type="text" name="search" placeholder="Search">
                     <button type="submit">
-                        <img src="./img/search_icon.png" alt="Image search icon" width="25px" height="25px"/>
+                        <img src="/PL_CMS/img/search_icon.png" alt="Image search icon" width="25px" height="25px"/>
                     </button>
                 </form>
                 <button id="btnLogin">Login</button>
