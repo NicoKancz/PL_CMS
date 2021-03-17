03af8fc98ab8b911fe890687bc7605010abe50c2
MrNico112 <MrNico112@users.noreply.github.com>
2021-03-17 18:05:59 +0100

    CRUD System: basic Create, Read, Update, Delete functions. Connected to the database PL_CMS.

--- crud/queries.php
+++ crud/queries.php
@@ -2,9 +2,26 @@
 require_once './database.php';
 
 //todo: create container
+function createContainer($conn, $name, $desc, $date, $languageId){
+    $query = "INSERT INTO containers(containerName,containerDescription,containerDate,languageId)
+                VALUES(' . $name . ',' . $desc . ',' . $date . ',' . $languageId . ')";
+    $conn->query($query);
+}
 
 //todo: read and show container
+function showContainers($conn){
+    $query = "SELECT * FROM containers";
+    $conn->query($query);
+}
 
 //todo: update container
+function updateContainer($conn, $id, $name, $desc, $date){
+    $query = "UPDATE containers SET containerName=$name, containerDescription=$desc, containerDate=$date WHERE containerId=$id";
+    $conn->query($query);
+}
 
 //todo: delete container
+function deleteContainer($conn, $id){
+    $query = 'DELETE FROM containers WHERE containerId=$id';
+    $conn->query($query);
+}
