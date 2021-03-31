5193327ce4c8f9b99f497e76c6ddfa54cb08a984
MrNico112 <MrNico112@users.noreply.github.com>
2021-03-31 16:21:41 +0200

    Finished adding article page and article list in theme page. Starting with updating article page.

--- updateArticle.php
+++ updateArticle.php
@@ -1,1 +1,9 @@
 <?php
+require_once './includes/queries.inc.php';
+require_once './classes/Article.php';
+require_once './includes/validation.inc.php';
+include_once './page_parts/header.php';
+?>
+<div class="center">
+    <img src="img/under-construction-yom.png" alt="Under construction"/>
+</div>
